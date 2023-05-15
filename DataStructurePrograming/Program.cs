39f49ecb857280f11fe0460e4e36517c0ff3f1a1
kashyapsudhir <kashyapsudhir1996@gmail.com>
2023-05-15 14:17:56 +0530

    Linked List Program

---
+++ DataStructurePrograming/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+ namespace DataStructurePrograming
+{
+    class Program
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("hello linked List Program");
+
+           
+        }
+      
+    }
+}
